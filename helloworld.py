8d3a79b3e224851809c6b6a4110aa29f68b2f6e6
Blaine <baweke4@gmail.com>
2017-07-13 09:34:09 -0700

    second commit

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+print("Hello World")
+from
+turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -250
+y_pos = -150
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+
+
+
+
+
+
+# Close window on click.
+exitonclick()
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security
